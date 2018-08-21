da5632feb018283aa49676cc2795a96fcccaf156
David Lacho <david.lacho@gmail.com>
2018-08-21 16:17:21 +0800

    Added customizable options & Rendering HTML of parent containers for bars

--- js/app.js
+++ js/app.js
@@ -1,31 +1,43 @@
 $(document).ready(() => {
 
   const drawBarChart = (data, options, element) => {
-    console.log(data);
-    console.log(options);
-    console.log(element);
+    const chartHeight = element.width();
+    const chartWidth = element.height();
+
+    // User customizable options. Default set below:
+    const {
+      barColour = "grey", labelColour = "black", barSpacing = 5, BarAxes = "x"
+    } = options;
 
     const numberOfBars = Object.keys(data).length;
 
+    // We want the bars to start on the left-ist side and end of the rightest side, with space in between each.
+    const numberOfSpaces = numberOfBars - 1;
+    const sizeOfBars = (chartWidth - (numberOfSpaces * barSpacing)) / numberOfBars;
+
+    let barHtml  = "";
+
     // Need for loop here to iterate through objects.
     // Based on the number of bars, it will iterate through the object
-
-    // User Customized options:
-    // Bar Colour
-    // Label Colour
-    // Bar spacing (space between bars)
-    // Bar Chart axes (not too sure yet! Come back to this)
-
     for (let i = 0; i < numberOfBars; i++) {
-      console.log(Object.keys(data)[i]);
+      barKey = Object.keys(data)[i];
+      barValue = Object.values(data)[i];
+      barHtml = `${barHtml}
+      <div class="bar-container">
+        <div class="bar-${i} bar">
+          ${barKey}: ${barValue}
+        </div>
+      </div>`;
       // for each item in the obj, it will pull the key and the value.
       // I'll have to make a string that will concatenate over the loop:
       // It will create a parent div that is the total height of the chart
       // Bar height should be dependent on the values of the data: The loop will create a div that will be the actual bar height. (So, for example if the value was 20/100, The height should be 20% of the parent div). it will be 100% of the width.
-      // Bar width should be dependent on the total amount of values passed. Bar spacing (space between bars) is customizable. So we need to calculate the size of the bars. We want the bars to start on the left-ist side and end of the rightest side, with space in between each.
+      // Bar width should be dependent on the total amount of values passed. Bar spacing (space between bars) is customizable. So we need to calculate the size of the bars.
 
       // I'll have to come up with a way to flip the axes of the chart.
     }
+
+    console.log(barHtml);
 
 
   };
@@ -41,7 +53,10 @@ $(document).ready(() => {
   };
 
   const sampleOptions = {
-    "Data": "Something"
+    "barColour": "blue",
+    "labelColour": "white",
+    "barSpacing": "5px",
+    "BarAxes": "x"
   };
 
   const sampleElement = $('.chart');
