d1fe14f8bd1703b1fc8fd3f82b4e0569b39d3371
David Lacho <david.lacho@gmail.com>
2018-08-21 15:27:36 +0800

    Setup drawBarChart function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
 $(document).ready(() => {
-  console.log('jQuery ready');
+
+  chartHeight = $('.chart').width();
+  chartWidth = $('.chart').height();
+
+  const drawChart = (data) => {
+    console.log(data);
+  };
+
+  const sampleData = {
+    "Play Sports": 45,
+    "Talk on Phone": 53,
+    "Visit With Friends": 99,
+    "Earn Money": 44,
+    "Chat Online": 66,
+    "School Clubs": 22,
+    "Watch TV": 37
+  };
+
+  drawChart(sampleData);
+
 });
